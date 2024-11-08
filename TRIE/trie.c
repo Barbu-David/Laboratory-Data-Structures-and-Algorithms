e38d0e54656313aa6cb1a9ec1218d5047b9dfcd6
Barbu David <barbu.david.c5a@student.ucv.ro>
2024-11-08 19:15:23 +0100

    added TRIE header

---
+++ TRIE/trie.c
@@ -0,0 +1,31 @@
+#include"trie.h"
+
+struct trie_node* trie_init(void)
+{
+
+}
+
+bool trie_empty(struct trie_node* root)
+{
+
+}
+
+void trie_print(struct trie_node* root)
+{
+
+}
+
+bool trie_search(struct trie_node* root, char* word, int word_size)
+{
+
+}
+
+void trie_insert(struct trie_node* root, char* word, int word_size)
+{
+
+}
+
+void trie_delete(struct trie_node* root, char* word, int word_size, int depth)
+{
+
+}
---
+++ TRIE/trie.h
@@ -0,0 +1,22 @@
+#ifndef TRIE_H
+#define TRIE_H
+
+#define ALPHABET_SIZE 27
+#include<stdbool.h>
+
+struct trie_node {
+	
+	struct trie_node* children[ALPHABET_SIZE];
+	bool end_flag;
+};
+
+struct trie_node* trie_init(void);
+
+bool trie_search(struct trie_node* root, char* word, int word_size);
+void trie_insert(struct trie_node* root, char* word, int word_size);
+void trie_delete(struct trie_node* root, char* word, int word_size, int depth);
+
+bool trie_empty(struct trie_node* root);
+void trie_print(struct trie_node* root);
+
+#endif
