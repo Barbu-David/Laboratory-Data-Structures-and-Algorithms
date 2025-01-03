3daa40408803d8d01491b5a61fb86fc2149f8d64
Barbu David <barbu.david.c5a@student.ucv.ro>
2025-01-03 20:30:17 +0100

    b_push - minor splitting bug in special case

--- B-TREES/b.c
+++ B-TREES/b.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<assert.h>
 #include<string.h>
+#include<stdio.h>
 
 struct b_node* b_init(void* value, size_t size) 
 {	
@@ -21,7 +22,6 @@ struct b_node* b_init(void* value, size_t size)
 
 	new_node->numb_v=1; //numb_v is a true counter, so for indexing it's off by one
 	new_node->values[0]=new_value;
-
 	return new_node;
 }
 
@@ -32,7 +32,8 @@ void b_split_child(struct b_node* root, int full_child_index)
 	struct b_node* sibling = malloc(sizeof(struct b_node)); //new node
 	sibling->values=malloc(sizeof(void*) * (2*T-1));
 	sibling->children=malloc(sizeof(struct b_node*) * (2*T));
-	
+	sibling->numb_v=T-1;
+
 	assert(sibling!=NULL && sibling->values!=NULL && sibling->children!=NULL);
 
 	sibling->leaf=child->leaf; //they have the same leaf value
@@ -40,12 +41,12 @@ void b_split_child(struct b_node* root, int full_child_index)
 	
 	int index;
 	//the sibling gets the large values
-	for(index=0; index<T-1; index++) 
+	for(index=0; index<=T-1; index++) 
 		sibling->values[index]=child->values[index+T];
 	
-	//and the children, if they are exist
+	//and the children, if they exist
 	if(!child->leaf) 
-		for(index=0; index<T; index++) 
+		for(index=0; index<=T; index++) 
 			sibling->children[index]=child->children[index+T];
 
 	child->numb_v=T-1; //the child keeps T-1 values. 
@@ -53,40 +54,85 @@ void b_split_child(struct b_node* root, int full_child_index)
 
 
 	//shift children of root to make room for the sibling
-	for(index=root->numb_v-1; index>full_child_index+1; index--)	//reverse order necessary
+	for(index=root->numb_v; index>=full_child_index+1; index--)	//reverse order necessary
 		root->children[index+1]=root->children[index];
 	root->children[full_child_index+1]=sibling;
 
 	//shift values of root to make room for median value
-	for(index=root->numb_v-2; index>full_child_index;index--)
-		root->values[index+1]=&root->values[index];
-	root->numb_v++;
-	root->values[full_child_index]=child->values[T];
+	for(index=root->numb_v-1; index>=full_child_index;index--)
+		root->values[index+1]=root->values[index];
 
+	root->values[full_child_index]=child->values[T-1];
+	root->numb_v++;
+}
+
+
+void b_split_root(struct b_node** root)
+{
+
+	struct b_node* new_root = malloc(sizeof(struct b_node));
+	assert(new_root!=NULL);
+
+	new_root->leaf=false;
+	new_root->values=malloc(sizeof(void*) * (2*T-1));
+	new_root->children=malloc(sizeof(struct b_node*) * (2*T));
+	
+	assert(new_root->values!=NULL && new_root->children!=NULL);
+
+	new_root->numb_v=0;
+
+	new_root->children[0]=*root;
+
+	b_split_child(new_root, 0);
+
+	*root=new_root;
+}
+
+
+void b_push_not_full(struct b_node** root, void* value, size_t size, bool (*check_equality)(void*, void*), void* (*compare)(void*, void*))
+{
+	assert(value!=NULL);
+	int index=(*root)->numb_v-1;
+
+	if((*root)->leaf) {
+
+		while(index>=0 && compare(value, (*root)->values[index]) != value) {
+			(*root)->values[index+1]=(*root)->values[index];
+			index--;
+		}
+
+		void* new_value=malloc(size);
+		assert(new_value!=NULL);
+		memcpy(new_value, value, size);
+		(*root)->values[index+1]=new_value;
+		(*root)->numb_v++;
+	}
+
+	else {
+		while(index>=0 && compare(value, (*root)->values[index]) != value) index--;
+		index++;
+		if((*root)->children[index]->numb_v==2*T-1) {
+			b_split_child(*root, index);
+			if(compare(value, (*root)->values[index])==value) index++;
+		}
+		b_push_not_full(&((*root)->children[index]), value, size, check_equality, compare);
+	}
+}
+
+void b_push(struct b_node** root, void* value, size_t size, bool (*check_equality)(void*, void*), void* (*compare)(void*, void*))
+{
+	if((*root)->numb_v==2*T-1) b_split_root(root);
+
+	b_push_not_full(root, value, size, check_equality, compare);
 }
 
 /*
-void b_split_root(struct b_node** root, void* value, size_t size, bool (*check_equalty)(void*, void*), void* (*compare)(void*, void*))
-{
-
-}
-
-
-
 void b_merge_siblings(struct b_node** root, void* value, size_t size, bool (*check_equalty)(void*, void*), void* (*compare)(void*, void*))
 {
 
 }
 
-void b_push_not_full(struct b_node** root, void* value, size_t size, bool (*check_equalty)(void*, void*), void* (*compare)(void*, void*))
-{
 
-}
-
-void b_push(struct b_node** root, void* value, size_t size, bool (*check_equalty)(void*, void*), void* (*compare)(void*, void*))
-{
-
-}
 void b_pop(struct b_node** root, void* value, size_t size, bool (*check_equaliy)(void*, void*), void* (*compare)(void*, void*))
 {
 
--- B-TREES/main.c
+++ B-TREES/main.c
@@ -1,7 +1,92 @@
+#include<stdio.h>
 #include"b.h"
+
+void* compare_for_ints(void* a, void* b)
+{
+        return *(int*)a >= *(int*)b? a:b;
+}
+
+bool check_equality_for_ints(void* a, void* b)
+{
+        return *(int*)a == *(int*)b;
+}
+
+void b_print(struct b_node* root)
+{	
+	int index;
+	for(index=0; index<=(root->numb_v)-1; index++)
+		printf("%d ", *((int*)(root->values[index])));
+	if(!root->leaf)
+		for(index=0; index<=(root->numb_v); index++) {
+			printf(" | ");
+			b_print(root->children[index]);
+	}
+}
+
 
 int main(void)
 {	
-	
+	int a =17;
+	struct b_node* root=b_init(&a, sizeof(int));
+	b_print(root);
+	printf("\n");
+
+	a=5;
+	b_push(&root, &a, sizeof(int), check_equality_for_ints, compare_for_ints);
+	b_print(root);
+	printf("\n");
+
+	a=21;
+	b_push(&root, &a, sizeof(int), check_equality_for_ints, compare_for_ints);
+	b_print(root);
+	printf("\n");
+
+	a=10;
+	b_push(&root, &a, sizeof(int), check_equality_for_ints, compare_for_ints);
+	b_print(root);
+	printf("\n");
+
+	a=32;
+	b_push(&root, &a, sizeof(int), check_equality_for_ints, compare_for_ints);
+	b_print(root);
+	printf("\n");
+
+	a=79;
+	b_push(&root, &a, sizeof(int), check_equality_for_ints, compare_for_ints);
+	b_print(root);
+	printf("\n");
+
+	a=8;
+	b_push(&root, &a, sizeof(int), check_equality_for_ints, compare_for_ints);
+	b_print(root);
+	printf("\n");
+
+	a=16;
+	b_push(&root, &a, sizeof(int), check_equality_for_ints, compare_for_ints);
+	b_print(root);
+	printf("\n");
+
+	a=7;
+	b_push(&root, &a, sizeof(int), check_equality_for_ints, compare_for_ints);
+	b_print(root);
+	printf("\n");
+
+	a=6;
+	b_push(&root, &a, sizeof(int), check_equality_for_ints, compare_for_ints);
+	b_print(root);
+	printf("\n");
+
+
+	a=20;
+	b_push(&root, &a, sizeof(int), check_equality_for_ints, compare_for_ints);
+	b_print(root);
+	printf("\n");
+
+	a=14;
+	b_push(&root, &a, sizeof(int), check_equality_for_ints, compare_for_ints);
+	b_print(root);
+	printf("\n");
+
+
 	return 0;
 }
